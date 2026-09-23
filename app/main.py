c2f4e2bc6e388c30c0de6630ea9af565f1e2ede2
Bohdan Vrulevsky <professor290997@gmail.com>
2026-09-23 17:45:25 +0300

    Created Elves and Dwarf classes subclasses 2

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
-def calculate_team_total_rating(s: list) -> int:
+def calculate_team_total_rating(players: list) -> int:
     total = 0
-    for el in s:
+    for el in players:
         result = el.get_rating()
         total += result
     return total
